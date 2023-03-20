d594318cf99eb4bd0207af061e14b9a820c64335
Verezub Olena <ussanel@ukr.net>
2023-03-20 21:00:24 +0200

    start

---
+++ js.js
@@ -0,0 +1,14 @@
+// Завдання1
+
+// Завдання2
+
+// Завдання3
+
+// Завдання4
+
+// Завдання5
+let mike =[ 'red', 'blue', 'yellow' ];
+let nikola =['cat', 'dog', 'bird'];
+let tom =['a', 'b', 'c', 'd'];
+ console.log (mike[2]);
+
